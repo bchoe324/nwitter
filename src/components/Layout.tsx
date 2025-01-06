62998e0cbcc2363080a6a1642b2d255bb616fa74
boramchoi <bchoe324@gmail.com>
2025-01-06 18:44:35 +0900

    💄 Home 페이지 레이아웃 css 수정, 입력 폼 css 작성

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,104 +5,135 @@ import { auth } from "../firebase";
 
 const Wrapper = styled.div`
   width: 100%;
-  display: grid;
-  grid-template-columns: auto 1fr;
+  min-width: 480px;
+  max-width: 820px;
+  display: flex;
+  position: relative;
+  border-left: 1px solid var(--gray);
+  border-right: 1px solid var(--gray);
+`;
+
+const Header = styled.header`
+  position: relative;
+  flex: 1 0 72px;
+  justify-content: center;
+  height: 100vh;
+  border-right: 1px solid var(--gray);
 `;
 
 const Menu = styled.nav`
-  position: sticky;
-  left: 0;
-  top: 0;
+  background-color: #000;
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 20px;
-  padding: 20px 10px;
-  background-color: #000;
+  padding: 20px;
 `;
 
 const MenuItem = styled.div`
-  width: 36px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
   svg {
+    width: 24px;
     fill: white;
+  }
+  :hover {
+    opacity: 0.9;
   }
   &.logout {
     cursor: pointer;
   }
+  @media screen and (max-width: 720px) {
+    span {
+      display: none;
+    }
+  }
 `;
 
 const Content = styled.div`
-  max-width: 600px;
-  overflow-x: auto;
-  margin: 0 auto;
+  flex: 5 1 300px;
+  position: relative;
+  height: 100vh;
+  overflow-y: auto;
 `;
 
 export default function Layout() {
   const nav = useNavigate();
   const onClickSubmit = async () => {
-    try {
-      await signOut(auth);
-      nav("/login");
-    } catch (error) {
-      console.error(error);
+    const isLoggingOut = confirm("Are you sure you want to log out?");
+    if (isLoggingOut) {
+      try {
+        await signOut(auth);
+        nav("/login");
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
   return (
     <Wrapper>
-      <Menu>
-        <MenuItem>
-          <Link to="/home">
+      <Header>
+        <Menu>
+          <MenuItem>
+            <Link to="/home">
+              <svg
+                dataSlot="icon"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                alt="home"
+              >
+                <path
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                  d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                />
+              </svg>
+            </Link>
+            <span>Home</span>
+          </MenuItem>
+          <MenuItem>
+            <Link to="/profile">
+              <svg
+                dataSlot="icon"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                alt="profile"
+              >
+                <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+              </svg>
+            </Link>
+            <span>Profile</span>
+          </MenuItem>
+          <MenuItem className="logout" onClick={onClickSubmit}>
             <svg
               dataSlot="icon"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
-              alt="home"
+              alt="log out"
             >
               <path
                 clipRule="evenodd"
                 fillRule="evenodd"
-                d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z"
+              />
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M19 10a.75.75 0 0 0-.75-.75H8.704l1.048-.943a.75.75 0 1 0-1.004-1.114l-2.5 2.25a.75.75 0 0 0 0 1.114l2.5 2.25a.75.75 0 1 0 1.004-1.114l-1.048-.943h9.546A.75.75 0 0 0 19 10Z"
               />
             </svg>
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/profile">
-            <svg
-              dataSlot="icon"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              alt="profile"
-            >
-              <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
-            </svg>
-          </Link>
-        </MenuItem>
-        <MenuItem className="logout" onClick={onClickSubmit}>
-          <svg
-            dataSlot="icon"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            alt="log out"
-          >
-            <path
-              clipRule="evenodd"
-              fillRule="evenodd"
-              d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z"
-            />
-            <path
-              clipRule="evenodd"
-              fillRule="evenodd"
-              d="M19 10a.75.75 0 0 0-.75-.75H8.704l1.048-.943a.75.75 0 1 0-1.004-1.114l-2.5 2.25a.75.75 0 0 0 0 1.114l2.5 2.25a.75.75 0 1 0 1.004-1.114l-1.048-.943h9.546A.75.75 0 0 0 19 10Z"
-            />
-          </svg>
-        </MenuItem>
-      </Menu>
+            <span>Log out</span>
+          </MenuItem>
+        </Menu>
+      </Header>
       <Content>
         <Outlet />
       </Content>
--- src/components/PostTweet.tsx
+++ src/components/PostTweet.tsx
@@ -2,31 +2,88 @@ import styled from "styled-components";
 
 const Form = styled.form`
   padding: 20px;
+  border-bottom: 1px solid var(--gray);
 `;
 
 const Textarea = styled.textarea`
   width: 100%;
+  height: 100px;
+  border: 0 none;
+  background-color: #000;
+  color: #fff;
   resize: none;
-  ::placeholder {
+  white-space: nowrap;
+  overflow-x: hidden;
+  &::placeholder {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
       Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
       sans-serif;
+    color: #999;
+    font-size: 16px;
+  }
+  &:focus {
+    outline: 0;
   }
 `;
 
-const AttachPhotoButton = styled.label``;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-const AttachPhotoInput = styled.input``;
+const AttachPhoto = styled.div`
+  input {
+    display: none;
+  }
+  label {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    svg {
+      width: 24px;
+      stroke: var(--primary-color);
+    }
+  }
+`;
 
-const SubmitButton = styled.input``;
+const SubmitButton = styled.input`
+  cursor: pointer;
+  background-color: var(--primary-color);
+  color: #fff;
+  padding: 8px 20px;
+  border: 0 none;
+  border-radius: 25px;
+`;
 
 export default function PostTweet() {
   return (
     <Form>
       <Textarea placeholder="What is happening?!"></Textarea>
-      <AttachPhotoButton htmlFor="file" />
-      <AttachPhotoInput type="file" id="file" />
-      <SubmitButton type="submit" value="Tweet" />
+      <ButtonWrapper>
+        <AttachPhoto>
+          <label htmlFor="file">
+            <svg
+              dataSlot="icon"
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              alt="add photo"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </label>
+          <input type="file" id="file" />
+        </AttachPhoto>
+        <SubmitButton type="submit" value="Tweet" />
+      </ButtonWrapper>
     </Form>
   );
 }
