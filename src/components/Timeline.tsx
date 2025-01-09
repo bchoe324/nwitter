3fb9963d29a602eeb09646df5b4e74ec7f33ff23
boramchoi <bchoe324@gmail.com>
2025-01-09 21:45:13 +0900

    ✅ 트윗 삭제 기능 추가

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -49,7 +49,6 @@ export default function Timeline() {
     };
     fetchTweets();
     return () => {
-      console.log("+++++++");
       unsubscribe && unsubscribe();
     };
   }, []);
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { ITweet } from "./Timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.article`
   display: flex;
@@ -14,7 +17,12 @@ const Wrapper = styled.article`
 `;
 
 const Collumn = styled.div`
-  line-height: 1.5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & > p {
+    line-height: 1.5;
+  }
   .image_wrapper {
     border-radius: 12px;
     overflow: hidden;
@@ -27,11 +35,72 @@ const Collumn = styled.div`
   }
 `;
 
-export default function Tweet({ name, imageUrl, text }: ITweet) {
+const DeleteButton = styled.button`
+  display: block;
+  cursor: pointer;
+  border: 0 none;
+  background: none;
+  height: 22px;
+  width: auto;
+  svg {
+    stroke: #fff;
+    height: 100%;
+    width: auto;
+  }
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+
+export default function Tweet({ name, imageUrl, text, uid, id }: ITweet) {
+  const user = auth.currentUser;
+
+  const onDelete = async () => {
+    const isConfirmed = confirm("Are you sure you want to delete this tweet?");
+
+    if (!isConfirmed || user?.uid !== uid) return;
+    try {
+      await deleteDoc(doc(db, "tweets", id));
+
+      if (imageUrl) {
+        const locationRef = ref(storage, `tweets/${uid}-${name}/${id}`);
+        await deleteObject(locationRef);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Wrapper>
-      <Collumn>{name}</Collumn>
-      <Collumn>{text}</Collumn>
+      <Collumn>
+        <div>{name}</div>
+        {user?.uid === uid ? (
+          <div>
+            <DeleteButton onClick={onDelete}>
+              <svg
+                alt="delete"
+                dataSlot="icon"
+                fill="none"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                />
+              </svg>
+            </DeleteButton>
+          </div>
+        ) : null}
+      </Collumn>
+      <Collumn>
+        <p>{text}</p>
+      </Collumn>
       <Collumn>
         <div className="image_wrapper">
           <img src={imageUrl} />
